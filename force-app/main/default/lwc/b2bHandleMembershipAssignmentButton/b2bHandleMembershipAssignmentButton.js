4dcd6ae6337166a1f08c5a09c7baf9cf9c10f18d
Poojitha <poojitha.anthati@stackIntelligence.com>
2025-01-23 00:37:49 +0530

    added custom button to unassign jobs manually

--- force-app/main/default/lwc/b2bHandleMembershipAssignmentButton/b2bHandleMembershipAssignmentButton.js
+++ force-app/main/default/lwc/b2bHandleMembershipAssignmentButton/b2bHandleMembershipAssignmentButton.js
@@ -1,5 +1,6 @@
 import { LightningElement, api } from 'lwc';
-import handleMembershipAssignmentManual from '@salesforce/apex/B2BSchooxAPIController.handleMembershipAssignmentManual';
+import handleJobAssignmentManual from '@salesforce/apex/B2BSchooxAPIController.handleJobAssignmentManual';
+import handleJobUnassignmentManual from '@salesforce/apex/B2BSchooxAPIController.handleJobUnassignmentManual';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class B2bHandleMembershipAssignmentButton extends LightningElement {
@@ -7,13 +8,23 @@ export default class B2bHandleMembershipAssignmentButton extends LightningElemen
     @api syncToStripeButton;
 
     handleAssignment() {
-        handleMembershipAssignmentManual({ recordId: this.recordId })
+        handleJobAssignmentManual({ recordId: this.recordId })
             .then(() => {
-                this.showToast('Success', 'Membership assignment processed successfully.', 'success');
+                this.showToast('Success', 'Job assignment processed successfully.', 'success');
             })
             .catch((error) => {
                 this.showToast('Error', error.body.message || 'An error occurred.', 'error');
             });
+    }
+
+    handleUnassignment() {
+        handleJobUnassignmentManual({ recordId: this.recordId })
+        .then(() => {
+            this.showToast('Success', 'Job Unassignment processed successfully.', 'success');
+        })
+        .catch((error) => {
+            this.showToast('Error', error.body.message || 'An error occurred.', 'error');
+        });
     }
 
     showToast(title, message, variant) {
